d84bcb004ceb48794e2cdffe22e118480820275c
Henry <carloshenry.wm@gmail.com>
2017-12-21 11:24:59 -0300

    Ready for usage! :D

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
 	$('#generate').click(function(){
-		var de  = $('#de').val()
-		var ate = $('#ate').val()
-		clean()
-		if(de != 0 && ate != 0){
-			console.log('DONE')
+		var de  = parseInt($('#de').val())
+		var ate = parseInt($('#ate').val())
+		cleanAndCreateTable()
+		if(ate > de) {
+			table = $('#content');
+			table.html('<tr></tr>')
+			br = 0;
+			for (var i = de; i <= ate; i++) {
+				if(br >= 20){
+					table.append('<tr></tr>');
+					br = 0;
+				}
+				$('#content tr:last-child').append("<td>"+i+"</td>");
+				br++;
+			}
+		}else{
+			location.reload(alert('VALORES INVÁLIDOS!'))
 		}
-
 	})
 });
 
 function cleanAndCreateTable(){
-	$('body').html("<table id='content' border='1'></table>");
+	$('body').html("<center><table id='content'></table></center>");
 }
